f900f952df046fbf0a6310c3be7d8502c5d62418
BluePsyduck <bluepsyduck@gmx.com>
2021-07-01 17:00:21 +0200

    Implemented updating the importTime after a combination has been imported.

--- test/src/Command/ImportCommandTest.php
+++ test/src/Command/ImportCommandTest.php
@@ -7,6 +7,8 @@ namespace FactorioItemBrowserTest\Api\Import\Command;
 use BluePsyduck\SymfonyProcessManager\ProcessManager;
 use BluePsyduck\SymfonyProcessManager\ProcessManagerInterface;
 use BluePsyduck\TestHelper\ReflectionTrait;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use FactorioItemBrowser\Api\Database\Entity\Combination;
 use FactorioItemBrowser\Api\Database\Repository\CombinationRepository;
 use FactorioItemBrowser\Api\Import\Command\ImportCommand;
@@ -28,112 +30,103 @@ use Symfony\Component\Console\Input\InputArgument;
  *
  * @author BluePsyduck <bluepsyduck@gmx.com>
  * @license http://opensource.org/licenses/GPL-3.0 GPL v3
- * @coversDefaultClass \FactorioItemBrowser\Api\Import\Command\ImportCommand
+ * @covers \FactorioItemBrowser\Api\Import\Command\ImportCommand
  */
 class ImportCommandTest extends TestCase
 {
     use ReflectionTrait;
 
-    /**
-     * The mocked combination repository.
-     * @var CombinationRepository&MockObject
-     */
-    protected $combinationRepository;
+    /** @var CombinationRepository&MockObject */
+    private CombinationRepository $combinationRepository;
+    /** @var Console&MockObject */
+    private Console $console;
+    /** @var EntityManagerInterface&MockObject */
+    private EntityManagerInterface $entityManager;
+    /** @var ExportDataService&MockObject */
+    private ExportDataService $exportDataService;
+    /** @var array<string, ImporterInterface> */
+    private array $importers = [];
+    private int $chunkSize = 42;
+    private int $numberOfParallelProcesses = 21;
 
-    /**
-     * The mocked console.
-     * @var Console&MockObject
-     */
-    protected $console;
-
-    /**
-     * The mocked export data service.
-     * @var ExportDataService&MockObject
-     */
-    protected $exportDataService;
-
-    /**
-     * Sets up the test case.
-     */
     protected function setUp(): void
     {
-        parent::setUp();
-
         $this->combinationRepository = $this->createMock(CombinationRepository::class);
         $this->console = $this->createMock(Console::class);
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
         $this->exportDataService = $this->createMock(ExportDataService::class);
     }
 
     /**
-     * Tests the constructing.
+     * @param array<string> $mockedMethods
+     * @return ImportCommand&MockObject
+     */
+    private function createInstance(array $mockedMethods = []): ImportCommand
+    {
+        return $this->getMockBuilder(ImportCommand::class)
+                    ->disableProxyingToOriginalMethods()
+                    ->onlyMethods($mockedMethods)
+                    ->setConstructorArgs([
+                        $this->combinationRepository,
+                        $this->console,
+                        $this->entityManager,
+                        $this->exportDataService,
+                        $this->importers,
+                        $this->chunkSize,
+                        $this->numberOfParallelProcesses,
+                    ])
+                    ->getMock();
+    }
+
+    /**
      * @throws ReflectionException
-     * @covers ::__construct
      */
     public function testConstruct(): void
     {
-        $importers = [
+        $this->importers = [
             'abc' => $this->createMock(ImporterInterface::class),
             'def' => $this->createMock(ImporterInterface::class),
         ];
-        $chunkSize = 42;
-        $numberOfParallelProcesses = 21;
 
-        $command = new ImportCommand(
-            $this->combinationRepository,
-            $this->console,
-            $this->exportDataService,
-            $importers,
-            $chunkSize,
-            $numberOfParallelProcesses,
+        $instance = $this->createInstance();
+
+        $this->assertSame($this->combinationRepository, $this->extractProperty($instance, 'combinationRepository'));
+        $this->assertSame($this->console, $this->extractProperty($instance, 'console'));
+        $this->assertSame($this->entityManager, $this->extractProperty($instance, 'entityManager'));
+        $this->assertSame($this->exportDataService, $this->extractProperty($instance, 'exportDataService'));
+        $this->assertSame($this->importers, $this->extractProperty($instance, 'importers'));
+        $this->assertSame($this->chunkSize, $this->extractProperty($instance, 'chunkSize'));
+        $this->assertSame(
+            $this->numberOfParallelProcesses,
+            $this->extractProperty($instance, 'numberOfParallelProcesses'),
         );
-
-        $this->assertSame($this->combinationRepository, $this->extractProperty($command, 'combinationRepository'));
-        $this->assertSame($this->console, $this->extractProperty($command, 'console'));
-        $this->assertSame($this->exportDataService, $this->extractProperty($command, 'exportDataService'));
-        $this->assertSame($importers, $this->extractProperty($command, 'importers'));
-        $this->assertSame($chunkSize, $this->extractProperty($command, 'chunkSize'));
-        $this->assertSame($numberOfParallelProcesses, $this->extractProperty($command, 'numberOfParallelProcesses'));
     }
 
     /**
-     * Tests the configure method.
      * @throws ReflectionException
-     * @covers ::configure
      */
     public function testConfigure(): void
     {
-        $command = $this->getMockBuilder(ImportCommand::class)
-                        ->onlyMethods(['setName', 'setDescription', 'addArgument'])
-                        ->setConstructorArgs([
-                            $this->combinationRepository,
-                            $this->console,
-                            $this->exportDataService,
-                            [],
-                            42,
-                            21,
-                        ])
-                        ->getMock();
-        $command->expects($this->once())
-                ->method('setName')
-                ->with($this->identicalTo(CommandName::IMPORT));
-        $command->expects($this->once())
-                ->method('setDescription')
-                ->with($this->isType('string'));
-        $command->expects($this->once())
-                ->method('addArgument')
-                ->with(
-                    $this->identicalTo('combination'),
-                    $this->identicalTo(InputArgument::REQUIRED),
-                    $this->isType('string')
-                );
+        $instance = $this->createInstance(['setName', 'setDescription', 'addArgument']);
+        $instance->expects($this->once())
+                 ->method('setName')
+                 ->with($this->identicalTo(CommandName::IMPORT));
+        $instance->expects($this->once())
+                 ->method('setDescription')
+                 ->with($this->isType('string'));
+        $instance->expects($this->once())
+                 ->method('addArgument')
+                 ->with(
+                     $this->identicalTo('combination'),
+                     $this->identicalTo(InputArgument::REQUIRED),
+                     $this->isType('string')
+                 );
 
-        $this->invokeMethod($command, 'configure');
+        $this->invokeMethod($instance, 'configure');
     }
 
     /**
-     * Tests the import method.
      * @throws ReflectionException
-     * @covers ::import
      */
     public function testImport(): void
     {
@@ -143,53 +136,46 @@ class ImportCommandTest extends TestCase
         $importer1 = $this->createMock(ImporterInterface::class);
         $importer2 = $this->createMock(ImporterInterface::class);
 
-        $importers = [
+        $this->importers = [
             'abc' => $importer1,
             'def' => $importer2,
         ];
 
-        $command = $this->getMockBuilder(ImportCommand::class)
-                        ->onlyMethods(['executeImporter', 'cleanup'])
-                        ->setConstructorArgs([
-                            $this->combinationRepository,
-                            $this->console,
-                            $this->exportDataService,
-                            $importers,
-                            42,
-                            21,
-                        ])
-                        ->getMock();
-        $command->expects($this->exactly(2))
-                ->method('executeImporter')
-                ->withConsecutive(
-                    [
-                        $this->identicalTo('abc'),
-                        $this->identicalTo($importer1),
-                        $this->identicalTo($exportData),
-                        $this->identicalTo($combination),
-                    ],
-                    [
-                        $this->identicalTo('def'),
-                        $this->identicalTo($importer2),
-                        $this->identicalTo($exportData),
-                        $this->identicalTo($combination),
-                    ],
-                );
+        $instance = $this->createInstance(['executeImporter', 'cleanup', 'updateImportTime']);
+        $instance->expects($this->exactly(2))
+                 ->method('executeImporter')
+                 ->withConsecutive(
+                     [
+                         $this->identicalTo('abc'),
+                         $this->identicalTo($importer1),
+                         $this->identicalTo($exportData),
+                         $this->identicalTo($combination),
+                     ],
+                     [
+                         $this->identicalTo('def'),
+                         $this->identicalTo($importer2),
+                         $this->identicalTo($exportData),
+                         $this->identicalTo($combination),
+                     ],
+                 );
+        $instance->expects($this->once())
+                 ->method('cleanup');
+        $instance->expects($this->once())
+                 ->method('updateImportTime')
+                 ->with($this->identicalTo($combination));
 
-        $this->invokeMethod($command, 'import', $exportData, $combination);
+        $this->invokeMethod($instance, 'import', $exportData, $combination);
     }
 
     /**
-     * Tests the executeImporter method.
      * @throws ReflectionException
-     * @covers ::executeImporter
      */
     public function testExecuteImporter(): void
     {
         $name = 'abc';
         $count = 100;
-        $chunkSize = 42;
-        
+        $this->chunkSize = 42;
+
         $exportData = $this->createMock(ExportData::class);
         $combination = $this->createMock(Combination::class);
 
@@ -201,11 +187,11 @@ class ImportCommandTest extends TestCase
         $importer->expects($this->once())
                  ->method('prepare')
                  ->with($this->identicalTo($combination));
-        
+
         $process1 = $this->createMock(ImportCommandProcess::class);
         $process2 = $this->createMock(ImportCommandProcess::class);
         $process3 = $this->createMock(ImportCommandProcess::class);
-        
+
         $processManager = $this->createMock(ProcessManagerInterface::class);
         $processManager->expects($this->exactly(3))
                        ->method('addProcess')
@@ -216,45 +202,32 @@ class ImportCommandTest extends TestCase
                        );
         $processManager->expects($this->once())
                        ->method('waitForAllProcesses');
-        
-        $command = $this->getMockBuilder(ImportCommand::class)
-                        ->onlyMethods(['createProcessManager', 'createSubProcess'])
-                        ->setConstructorArgs([
-                            $this->combinationRepository,
-                            $this->console,
-                            $this->exportDataService,
-                            [],
-                            $chunkSize,
-                            21,
-                        ])
-                        ->getMock();
-        $command->expects($this->once())
-                ->method('createProcessManager')
-                ->willReturn($processManager);
-        $command->expects($this->exactly(3))
-                ->method('createSubProcess')
-                ->withConsecutive(
-                    [$this->identicalTo($combination), $this->identicalTo($name), $this->identicalTo(0)],
-                    [$this->identicalTo($combination), $this->identicalTo($name), $this->identicalTo(1)],
-                    [$this->identicalTo($combination), $this->identicalTo($name), $this->identicalTo(2)],
-                )
-                ->willReturnOnConsecutiveCalls(
-                    $process1,
-                    $process2,
-                    $process3,
-                );
 
-        $this->invokeMethod($command, 'executeImporter', $name, $importer, $exportData, $combination);
+        $instance = $this->createInstance(['createProcessManager', 'createSubProcess']);
+        $instance->expects($this->once())
+                 ->method('createProcessManager')
+                 ->willReturn($processManager);
+        $instance->expects($this->exactly(3))
+                 ->method('createSubProcess')
+                 ->withConsecutive(
+                     [$this->identicalTo($combination), $this->identicalTo($name), $this->identicalTo(0)],
+                     [$this->identicalTo($combination), $this->identicalTo($name), $this->identicalTo(1)],
+                     [$this->identicalTo($combination), $this->identicalTo($name), $this->identicalTo(2)],
+                 )
+                 ->willReturnOnConsecutiveCalls(
+                     $process1,
+                     $process2,
+                     $process3,
+                 );
+
+        $this->invokeMethod($instance, 'executeImporter', $name, $importer, $exportData, $combination);
     }
 
     /**
-     * Tests the createProcessManager method.
      * @throws ReflectionException
-     * @covers ::createProcessManager
      */
     public function testCreateProcessManager(): void
     {
-        $parallelProcesses = 42;
         $process = $this->createMock(ImportCommandProcess::class);
 
         $this->console->expects($this->exactly(2))
@@ -264,24 +237,17 @@ class ImportCommandTest extends TestCase
                           [$this->identicalTo('Processing chunk 2')],
                       );
 
-        $command = $this->getMockBuilder(ImportCommand::class)
-                        ->onlyMethods(['handleProcessFinish'])
-                        ->setConstructorArgs([
-                            $this->combinationRepository,
-                            $this->console,
-                            $this->exportDataService,
-                            [],
-                            42,
-                            $parallelProcesses,
-                        ])
-                        ->getMock();
-        $command->expects($this->once())
+        $instance = $this->createInstance(['handleProcessFinish']);
+        $instance->expects($this->once())
                 ->method('handleProcessFinish')
                 ->with($this->identicalTo($process));
 
         /* @var ProcessManager $result */
-        $result = $this->invokeMethod($command, 'createProcessManager');
-        $this->assertSame($parallelProcesses, $this->extractProperty($result, 'numberOfParallelProcesses'));
+        $result = $this->invokeMethod($instance, 'createProcessManager');
+        $this->assertSame(
+            $this->numberOfParallelProcesses,
+            $this->extractProperty($result, 'numberOfParallelProcesses'),
+        );
 
         $startCallback = $this->extractProperty($result, 'processStartCallback');
         $this->assertIsCallable($startCallback);
@@ -294,9 +260,7 @@ class ImportCommandTest extends TestCase
     }
 
     /**
-     * Tests the handleProcessFinish method.
      * @throws ReflectionException
-     * @covers ::handleProcessFinish
      */
     public function testHandleProcessFinish(): void
     {
@@ -314,22 +278,12 @@ class ImportCommandTest extends TestCase
                       ->method('writeData')
                       ->with($this->identicalTo($output));
 
-        $command = new ImportCommand(
-            $this->combinationRepository,
-            $this->console,
-            $this->exportDataService,
-            [],
-            42,
-            21,
-        );
-
-        $this->invokeMethod($command, 'handleProcessFinish', $process);
+        $instance = $this->createInstance();
+        $this->invokeMethod($instance, 'handleProcessFinish', $process);
     }
 
     /**
-     * Tests the handleProcessFinish method.
      * @throws ReflectionException
-     * @covers ::handleProcessFinish
      */
     public function testHandleProcessFinishWithError(): void
     {
@@ -343,22 +297,12 @@ class ImportCommandTest extends TestCase
 
         $this->expectException(CommandFailureException::class);
 
-        $command = new ImportCommand(
-            $this->combinationRepository,
-            $this->console,
-            $this->exportDataService,
-            [],
-            42,
-            21,
-        );
-
-        $this->invokeMethod($command, 'handleProcessFinish', $process);
+        $instance = $this->createInstance();
+        $this->invokeMethod($instance, 'handleProcessFinish', $process);
     }
 
     /**
-     * Tests the createSubProcess method.
      * @throws ReflectionException
-     * @covers ::createSubProcess
      */
     public function testCreateSubProcess(): void
     {
@@ -368,7 +312,6 @@ class ImportCommandTest extends TestCase
 
         $part = 'abc';
         $chunk = 3;
-        $chunkSize = 42;
 
         $expectedResult = new ImportCommandProcess(
             CommandName::IMPORT_PART,
@@ -376,24 +319,14 @@ class ImportCommandTest extends TestCase
             ['abc', '126', '42'],
         );
 
-        $command = new ImportCommand(
-            $this->combinationRepository,
-            $this->console,
-            $this->exportDataService,
-            [],
-            $chunkSize,
-            21,
-        );
-
-        $result = $this->invokeMethod($command, 'createSubProcess', $combination, $part, $chunk);
+        $instance = $this->createInstance();
+        $result = $this->invokeMethod($instance, 'createSubProcess', $combination, $part, $chunk);
 
         $this->assertEquals($expectedResult, $result);
     }
 
     /**
-     * Tests the cleanup method.
      * @throws ReflectionException
-     * @covers ::cleanup
      */
     public function testCleanup(): void
     {
@@ -404,20 +337,33 @@ class ImportCommandTest extends TestCase
         $importer2->expects($this->once())
                   ->method('cleanup');
 
-        $importers = [
+        $this->importers = [
             'abc' => $importer1,
             'def' => $importer2,
         ];
 
-        $command = new ImportCommand(
-            $this->combinationRepository,
-            $this->console,
-            $this->exportDataService,
-            $importers,
-            42,
-            21,
-        );
+        $instance = $this->createInstance();
+        $this->invokeMethod($instance, 'cleanup');
+    }
 
-        $this->invokeMethod($command, 'cleanup');
+    /**
+     * @throws ReflectionException
+     */
+    public function testUpdateImportTime(): void
+    {
+        $combination = $this->createMock(Combination::class);
+        $combination->expects($this->once())
+                    ->method('setImportTime')
+                    ->with($this->isInstanceOf(DateTime::class));
+
+        $this->entityManager->expects($this->once())
+                            ->method('persist')
+                            ->with($this->identicalTo($combination));
+        $this->entityManager->expects($this->once())
+                            ->method('flush');
+
+        $instance = $this->createInstance();
+
+        $this->invokeMethod($instance, 'updateImportTime', $combination);
     }
 }
